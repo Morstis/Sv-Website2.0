336ff8c0d3c3be074fbd1c2d7674ddc841454def
Lucas Wiese <mors@Mors-PC.fritz.box>
2020-05-01 14:46:39 +0200

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBl5cS4WbDKR0poZP_Z0wbHbC8X9cpvQ0Q',
+    authDomain: 'sv-website3-lwp1251.firebaseapp.com',
+    databaseURL: 'https://sv-website3-lwp1251.firebaseio.com',
+    projectId: 'sv-website3-lwp1251',
+    storageBucket: 'sv-website3-lwp1251.appspot.com',
+    messagingSenderId: '369707805200',
+    appId: '1:369707805200:web:97c820d14e18e2f9bdf895',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBl5cS4WbDKR0poZP_Z0wbHbC8X9cpvQ0Q',
+    authDomain: 'sv-website3-lwp1251.firebaseapp.com',
+    databaseURL: 'https://sv-website3-lwp1251.firebaseio.com',
+    projectId: 'sv-website3-lwp1251',
+    storageBucket: 'sv-website3-lwp1251.appspot.com',
+    messagingSenderId: '369707805200',
+    appId: '1:369707805200:web:97c820d14e18e2f9bdf895',
+  },
 };
 
 /*
